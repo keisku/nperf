1e81bdbb269f3430f65ede99b359d26ed3ae0a70
keisku <keisuke.umegaki.630@gmail.com>
2023-08-05 07:20:08 +0000

    add fentry PoC

---
+++ ebpf/ebpf.go
@@ -0,0 +1,4 @@
+package ebpf
+
+// $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS -type event bpf ./c/fentry.c -- -I./c
